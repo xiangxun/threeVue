f14796e0091773d844aa48a35a384b619a798c58
xiangxun <1276205568@qq.com>
2021-11-13 20:14:42 +0800

    20211113

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
 
     <div id="info">
-      xiangxun
+      
+      <a href="https://xiangxun.github.io/" target="_blank" rel="noopener">xiangxun</a>
+      |
       <a href="https://github.com/xiangxun/threeVue" target="_blank" rel="noopener">github</a>
-      /
-      <a href="https://space.bilibili.com/314030602" target="_blank" rel="noopener">bilibili</a>
+      <!-- /
+      <a href="https://space.bilibili.com/314030602" target="_blank" rel="noopener">bilibili</a> -->
     </div>
 
 
-  <div class="three-canvas" ref="threeTarget">
+  <div class="three-canvas" ref="threeTarget"></div>
 
-
-
-  </div>
+    <div  id="icp">
+      ICP证:
+      <a href="https://beian.miit.gov.cn/" target="_blank">赣ICP备2021009921号-1</a>
+    </div>
 </template>
 
 
